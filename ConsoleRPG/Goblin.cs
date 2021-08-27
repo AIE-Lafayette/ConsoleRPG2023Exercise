0426151d586166f2ed1d0af04affed59e1ee3bf9
Lodis <lodis.perkins@aie.edu>
2021-08-27 16:56:55 -0500

    Added character classes

---
+++ ConsoleRPG/Goblin.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleRPG
+{
+    class Goblin
+    {
+        private float _health;
+        private float _damage;
+        private float _defense;
+        private float _power;
+        private float _name;
+
+        public float TakeDamage(float damage)
+        {
+            float damageTaken = damage - _defense;
+
+            if (damageTaken < 0)
+            {
+                damageTaken = 0;
+            }
+
+            _health -= damageTaken;
+            return damageTaken;
+        }
+
+
+    }
+}
---
+++ ConsoleRPG/Player.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleRPG
+{
+    class Player
+    {
+        private string _name;
+        private float _health;
+        private float _defense;
+        private float _power;
+        private Item _currentItem;
+
+        public Player(string name, float health, float defense, float power)
+        {
+            _name = name;
+            _health = health;
+            _defense = defense;
+            _power = power;
+        }
+        
+        public float TakeDamage(float damage)
+        {
+            float damageTaken = damage - _defense;
+
+            if (damageTaken < 0)
+            {
+                damageTaken = 0;
+            }
+
+            _health -= damageTaken;
+            return damageTaken;
+        }
+    }
+}
