d26db49cacab990631111e3ec17fe5a1872106fb
Lodis <lodis.perkins@aie.edu>
2021-08-27 14:08:15 -0500

    Added starting template

---
+++ ConsoleRPG/Game.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleRPG
+{
+    class Game
+    {
+        private bool _gameOver;
+
+        public void Start()
+        {
+            
+        }
+
+        public void Update()
+        {
+
+        }
+
+        public void End()
+        {
+
+        }
+
+        public void Run()
+        {
+            Start();
+
+            while (!_gameOver)
+            {
+                Update();
+            }
+
+            End();
+        }
+
+    }
+}
